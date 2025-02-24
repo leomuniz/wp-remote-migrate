5dda67fd75ee9c2bc36c9f40c5d03f23b63bb54a
Léo Muniz <leomuniz.primos@gmail.com>
2025-02-24 20:44:16 -0300

    Removes values from default variables

--- wp-remote-migrate-default.sh
+++ wp-remote-migrate-default.sh
@@ -1,5 +1,5 @@
 # default_config.sh
-DEFAULT_MAX_SIZE="2m"
-DEFAULT_EXCLUDED_EXTENSIONS="mp4 wmv mkv avi pdf log zip"
-DEFAULT_TABLES_TO_EXCLUDE="wp_wsal_occurrences,wp_wsal_metadata,wp_actionscheduler_actions,wp_actionscheduler_claims,wp_actionscheduler_groups,wp_actionscheduler_logs" 
-DEFAULT_PLUGINS_TO_DEACTIVATE="login-lockdown wp-2fa wp-security-audit-log wp-seopress wp-seopress-pro wp-migrate-db-pro wp101"
+DEFAULT_MAX_SIZE=""
+DEFAULT_EXCLUDED_EXTENSIONS=""
+DEFAULT_TABLES_TO_EXCLUDE=""
+DEFAULT_PLUGINS_TO_DEACTIVATE=""
